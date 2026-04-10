0535f9ee1491dd5a02756ae1ba6c366660fae820
Hrushikesh Paraye <parayehrushikesh10@gmail.com>
2026-04-10 08:31:08 +0530

    Update login API endpoint to production URL

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
         e.preventDefault() 
         // setEmail("")
         try {
-            const res = await axios.post("http://localhost:3000/api/auth/login", {  
+            const res = await axios.post("https://hotel-booking-backend-deployment-ddgh.onrender.com/api/auth/login", {  
                 email,
                 password
             })
